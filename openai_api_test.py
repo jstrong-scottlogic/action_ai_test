bdb74a809d3ce68ad5934cb9ca494ecdfa7e6a90
James <jstrong@scottlogic.com>
2024-03-22 15:06:32 +0000

    Cleaned up

--- openai_api_test.py
+++ openai_api_test.py
@@ -5,7 +5,7 @@ import sys
 with open("blog_text.txt", "r") as file:
     text = file.read()
 
-prompt = f"""You are a copy editor at a prestigious British newspaper.
+PROMPT = f"""You are a copy editor at a prestigious British newspaper.
             Please find spelling and grammar errors, subject verb disagreements, and tense errors and provide corrections for the text delimited by ***.
             If there are no corrections to be made, say so. I prefer British English spelling.
             Please only arrange the errors, reasons, and corrections concisely into a markdown table with the changes highlighted in bold.
@@ -17,7 +17,7 @@ prompt = f"""You are a copy editor at a prestigious British newspaper.
             ***
             
             """
-model = "gpt-3.5-turbo"
+MODEL = "gpt-3.5-turbo"
 
 
 def format(markdown_text):
@@ -28,9 +28,9 @@ def format(markdown_text):
 def error_check():
     try:
         api_key = sys.argv[1]
-        client = OpenAI(api_key=sys.argv[1])
+        client = OpenAI(api_key=api_key)
         completion = client.chat.completions.create(
-            model=model, messages=[{"role": "user", "content": prompt}]
+            model=MODEL, messages=[{"role": "user", "content": PROMPT}]
         )
     except Exception as e:
         print(f"An error occurred: {e}")
